9b71c15f2dc2548ec37e491de08bc06095c5ff08
sachinlv <nicaslv@gmail.com>
2015-04-20 20:32:53 +0200

    code modified

--- singlebytexor.py
+++ singlebytexor.py
@@ -1,31 +1,33 @@
 __author__ = 'freak'
 import string
 
-input_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-
+inptutstr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+hexlist = "0123456789abcdef"
+charlist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 #How?
 #Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric.
 #Evaluate each output and choose the one with the best score.
-bin_str = ''
-key_str = string.ascii_letters + string.digits #'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+hexxor = []
+charxor = []
 
-for char in input_string:
-    bin_str = bin_str + bin(int(char, 16)).split('b')[1]
-
-#print(len(bin_str) % 7)
-if len(bin_str) % 7 != 0:
-    bin_str = bin_str.ljust(len(bin_str) + (7 - len(bin_str) % 7), '0')
-
-bin_str_tmp = bin_str
-for key in key_str:
-    bin_str = bin_str_tmp
-    tmp = ''
-    char_str = ''
-    while len(bin_str) > 0:
-        tmp = bin_str[0:6]
-        bin_str = bin_str[6:]
-        tmp = int(tmp, 2) ^ ord(key)
-        char_str = char_str + chr(tmp)
+def decodehex():
+    for c in range(0,15):
+        a = ''
+        out = [a + str(int(hexlist[c],16) ^ int(inptutstr[i], 16)) for i in range(0,len(inptutstr)-1)]
+        hexxor.append((''.join(out)))
+        
 
 
-    print(char_str)
+def decodeascii():
+    for c in range(0, len(charlist)-1):
+        a = ''
+        out = [a + str(ord(charlist[c]) ^ int(inptutstr[i], 16)) for i in range(0,len(inptutstr)-1)]
+        charxor.append(''.join(out))
+
+
+
+
+if __name__ == '__main__':
+    decodehex()
+    decodeascii()
+    print(hexxor)
